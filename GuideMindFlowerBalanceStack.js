cfda31a7d3ba690ddbccba0886615b632be5bc8d
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-12-23 13:20:20 +0200

    fix data

--- GuideMindFlowerBalanceStack.js
+++ GuideMindFlowerBalanceStack.js
@@ -333,7 +333,7 @@ const BlossomNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-12-21T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-12-25T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
